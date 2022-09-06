486c0d37482a00910ebcf1af0a2c2e8344a388ea
kangheechoul <rkdsus123com@naver.com>
2022-09-06 18:21:23 +0900

    상단 고정 메뉴 추가

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState }from "react";
 import { Link } from 'react-router-dom';
 import  HeaderService  from '../service/header';
-import { Container, Nav, Navbar, NavDropdown, NavItem } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown, NavItem, Form, Button,
+Row,
+} from "react-bootstrap";
 
 import "../assets/css/header.css";
 
@@ -15,6 +17,7 @@ export const Header = () =>{
     const viewMenus = [];
     
     // 보여줄 메뉴
+    const [viewFlag, setViewFlag] = useState("none");
     const [viewMenu, setViewMenu] = useState([]);
 
     // 헤더 메뉴 가져오기
@@ -28,62 +31,77 @@ export const Header = () =>{
 
     // 메뉴 클릭시 보여질 하위 메뉴들
     const menu_view = (parent_idx) => {
-        console.log(viewMenus[parent_idx]);
+        if(viewMenus[parent_idx].key === viewMenu.key){
+            
+            setViewFlag(viewFlag == "block" ? "none" : "block");
+        }else{
+            setViewFlag("block");
+            setViewMenu(viewMenus[parent_idx]);
+        }
     }
 
     // 헤더 메뉴 html 태그 화 하기
-    let menu_list = data.map((a)=>{
+    data.map((a,b)=>{
         let group_title = a.title;
         let group_idx = a.idx;
-        menuTitles.push(<Nav.Link >{group_title}</Nav.Link>);
+        menuTitles.push(<Nav.Link key={group_idx} href ={"#menu_group"+group_idx} onClick={()=>{menu_view(b)}}>{group_title}</Nav.Link>);
 
-        let li = a.list.map((aa)=>{
-            return <li key={aa.idx}>{aa.name}</li>;
+        let menu_list = a.list.map((aa)=>{
+            return <span key={aa.idx}>{aa.name}</span>;
         });
 
-        // return <li key={a.idx}>{group_title}<ul style ={{display:"block"}}>{li}</ul></li>;
-        return <ul style ={{display:"block"}}>{li}</ul>;
-
-
-
+        viewMenus.push(<div key={group_idx}>{menu_list}</div>);
 
     });
-    
-    
 
     return (
         <>
-        <div className={"MainHead"}>
+        <Container>
+            <div className={"MainHead"}>
 
-        <div className={"HeadLink"}>
-            <Link to={"/"}>로그인</Link> |
-            <Link to={"/"}>회원가입</Link> |
-            <Link to={"/"}>공지사항</Link> |
-            <Link to={"/"}>이벤트</Link>
-        </div>
+                <div className={"HeadLink"}>
+                    <Link to={"/"}>로그인</Link> |
+                    <Link to={"/"}>회원가입</Link> |
+                    <Link to={"/"}>공지사항</Link> |
+                    <Link to={"/"}>이벤트</Link>
+                </div>
 
-        <div className={"HeadLogo"}>
-            <img src={"./assets/image/dog.jpg"}/>
-        </div>
-        
-        <Navbar bg={"primary"} variant={"dark"} style={{padding:"0.5% 2%",marginTop:"2%"}}>
-            <Navbar.Brand>Menu</Navbar.Brand>
-            <Nav className={""}>
-                {menuTitles}
-                {/* <Nav.Link href={"#11"}>메뉴1</Nav.Link>
-                <Nav.Link href={"#22"}>메뉴2</Nav.Link>
-                <Nav.Link href={"#33"}>메뉴3</Nav.Link> */}
-            </Nav>
-            
-            
-        </Navbar>
-        <div style={{display:"block"}}>
-            {menu_list}
-        </div>
-            
+                <Row>
+                    <div className={"HeadLogo"}>
+                        <img src={"./assets/image/dog.jpg"}/>
+                    </div>
+                </Row>
+                <Row>
+                    <Navbar bg={"primary"} variant={"dark"} expand={"lg"} style={{padding:"0.5% 2%",marginTop:"2%"}}>
+                    <Navbar.Brand>Menu</Navbar.Brand>
+                    <Navbar.Collapse>
+                        <Nav className="me-auto my-2 my-lg-1">
+                            {menuTitles}
+                        </Nav>
+                        
+                        <Form className="d-flex">
+                            <Form.Control
+                            type="search"
+                            placeholder="검색어"
+                            className="me-2"
+                            aria-label="Search"
+                            />
+                        <Button variant="light" style={{maxWidth:"80px", minWidth:"60px"}}>검색</Button>
+                    </Form>
+                    </Navbar.Collapse>
+                        
+                    </Navbar>
+                </Row>
                 
-
-        </div>
+                <Row >
+                    <div className={"HeadMenuList"} style={{display:viewFlag}}>
+                        {viewMenu}
+                    </div>
+                </Row>
+                
+            </div>
+        </Container>
+        
         </>
     );
 }
