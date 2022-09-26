8a2e666b50fa8fe6a736379c40b42fb72189ffa0
kangheechoul <71624050+kangheechoul@users.noreply.github.com>
2022-09-27 01:59:38 +0900

    박스리스트 수정
    css로 스타일 옮김 필요

--- src/components/boxList.js
+++ src/components/boxList.js
@@ -19,27 +19,51 @@ export const BoxList = ({title, data}) =>{
   return (
       <>
        {/* 내 정보 박스 */}
-      <Box sx={{fontSize:"24px", width:"100%", height:"5%", borderBottom:"2px solid black",fontWeight:"bold", backgroundColor:"white"}}>
+      <Box sx={{fontSize:"24px", display:"inline-block",width:"100%", height:"5%", borderBottom:"2px solid black",fontWeight:"bold", backgroundColor:"white"}}>
         {viewTitle}
       </Box>
 
       {/* 박스형 게시글 리스트 */}
       <Box className={"boxList"} sx={{minHeight:"95%" , backgroundColor:"#dcdcdc"}}>
         
-        <Grid container  sx={{pt:"1%", maxHeight:"100%", minHeight:"95%", px:"2%", py:"1%" }}>
+        <Grid container spacing={2} sx={{pt:"1%", maxHeight:"100%", minHeight:"95%", px:"2%", py:"1%" }}>
 
           {/* 아이템 */}
-          <Grid item xs={12} sm={6} sx={{maxHeight:"15vh", minHeight:"15vh",padding:"1%", backgroundColor:"red"}}>
-            <Card sx={{backgroundColor:"white", width:"100%", maxHeight:"100%", minHeight:"100%"}}>
-              <Grid container sx={{minHeight:"100%"}}>
-                <Grid item sm={3} xs={3} sx={{backgroundColor:"yellowgreen", minHeight:"100%", padding:"2%"}}>
-                  {/* <img src={Logo} style={{width:"96%", maxHeight:"100vh"}}/> */}
+          <Grid item xs={12} sm={6}>
+              <Grid container sx={{padding:"1%", backgroundColor:"white", borderRadius:"12px", boxShadow:"3px 3px 3px 1px gray", maxHeight:"inherit"}}>
+                <Grid item sm={3} xs={3} sx={{maxHeight:"inherit", margin:"auto 0", padding:"2%", textAlign:"center"}}>
+                  <img src={Logo} style={{width:"80%", height:"80%", borderRadius:"10%"}}/>
                 </Grid>
-                <Grid item sm={9} xs={9} sx={{backgroundColor:"green",minHeight:"100%"}}>
-                  xxx
+                <Grid item sm={9} xs={9} sx={{maxHeight:"inherit"}}>
+                  <Grid container sx={{pt:"5%"}}>
+                    <Grid item sm={6} xs={6} sx={{px:"2%", overflow:"hidden", textOverflow:"ellipsis", whiteSpace:"nowrap", fontWeight:"bold"}}>
+                      타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀
+                    </Grid>
+                    <Grid item sm={2} xs={2} sx={{textAlign:"left", fontWeight:"bold"}}>
+                      3
+                    </Grid>
+                    <Grid item sm={2} xs={2} sx={{fontWeight:"bold"}}>
+                      {/* new 아이콘 */}
+                      {/* <Box sx={{textAlign:"center", backgroundColor:"red", color:"white",padding:"0 5%", borderRadius:"5%"}}>
+                        new
+                      </Box> */}
+                      {/* 빨간 배경 숫자 아이콘 */}
+                      <Box sx={{textAlign:"center", backgroundColor:"red", color:"white", width:"50%",borderRadius:"50%"}}>
+                        3
+                      </Box>
+                    </Grid>
+                    <Grid item sm={2} xs={2} sx={{textAlign:"center"}}>
+                      13:13
+                    </Grid>
+                  </Grid>
+
+                  <Grid container sx={{pt:"10%"}}>
+                    <Grid item sm={8} xs={8} sx={{px:"2%", overflow:"hidden", textOverflow:"ellipsis", whiteSpace:"nowrap", color:"gray"}}>
+                      내용내용내용내용내용내용내용내용내용내용
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Card>
           </Grid>
           
 
